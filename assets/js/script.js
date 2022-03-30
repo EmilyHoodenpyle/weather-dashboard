ac0be733765f5c379599e153a79a674785779058
Emily Hoodenpyle <emilyghoodenpyle@gmail.com>
2022-03-29 17:22:33 -0700

    modified js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
 var city = document.querySelector('search-input');
 var APIKey = '8139c3d54584e911194f90d49ebbcf0c';
+var searchFormEl = document.querySelector('search-form')
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-fetch(queryURL);
+
+function searchApi(city) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    console.log(queryURL)
+
+    fetch(queryURL)
+        .then(function(response) {
+            if(!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function (locRes) {
+
+        });
+}
+
+function printResults(resultObj) {
+    console.log(resultObj);
+    var resultCard = document.createElement('div');
+    resultCard.className.add('card');
+
+    var resultBody = document.createElement('div');
+    resultBody.className.add('card-body');
+    resultCard.append(resultBody);
+
+    
+}
